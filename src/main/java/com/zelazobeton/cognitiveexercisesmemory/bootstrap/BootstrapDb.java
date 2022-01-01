e95e28616e617a1d179cb3dc99fe9b80fec90ba4
Franciszek Bubnicki <franciszek.bubnicki@sollers.eu>
2022-01-01 20:39:05 +0100

    Add bootstrap functionality & memory imgs

---
+++ src/main/java/com/zelazobeton/cognitiveexercisesmemory/bootstrap/BootstrapDb.java
@@ -0,0 +1,55 @@
+package com.zelazobeton.cognitiveexercisesmemory.bootstrap;
+
+import static com.zelazobeton.cognitiveexercisesmemory.constants.FileConstants.MEMORY_IMG_FOLDER;
+import static com.zelazobeton.cognitiveexercisesmemory.constants.FileConstants.MEMORY_IMG_PATH;
+import static com.zelazobeton.cognitiveexercisesmemory.constants.FileConstants.VERSION_1;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import com.zelazobeton.cognitiveexercisesmemory.domain.MemoryImg;
+import com.zelazobeton.cognitiveexercisesmemory.repository.MemoryImgRepository;
+import com.zelazobeton.cognitiveexercisesmemory.services.ResourceService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+@Profile("bootstrap")
+public class BootstrapDb implements CommandLineRunner {
+    @Value("${server-host}")
+    private String serverHost;
+    @Value("${server.port}")
+    private String serverPort;
+    private String serverAddress = this.serverHost + ":" + this.serverPort;
+    private final MemoryImgRepository memoryImgRepository;
+    private final ResourceService resourceService;
+
+    @Override
+    public void run(String... args) {
+        this.loadMemoryImages();
+    }
+
+    private void loadMemoryImages() {
+        Path memoryImagesFolder = this.resourceService.getPath(MEMORY_IMG_FOLDER);
+        List<MemoryImg> memoryImgs = new ArrayList<>();
+        for(File file: Objects.requireNonNull(memoryImagesFolder.toFile().listFiles())) {
+            if (!file.isDirectory()) {
+                String imgAddress = this.serverAddress + VERSION_1 + MEMORY_IMG_PATH + file.getName();
+                memoryImgs.add(MemoryImg.builder().address(imgAddress).build());
+            }
+        }
+        this.memoryImgRepository.saveAll(memoryImgs);
+    }
+
+}
--- src/main/java/com/zelazobeton/cognitiveexercisesmemory/domain/MemoryImg.java
+++ src/main/java/com/zelazobeton/cognitiveexercisesmemory/domain/MemoryImg.java
@@ -3,12 +3,14 @@ package com.zelazobeton.cognitiveexercisesmemory.domain;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Document("memoryImgs")
